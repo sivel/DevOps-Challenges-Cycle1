141754209e7b1249aca6478eb04a793233cfa5b2
Matt Martz <matt@sivel.net>
2014-01-02 13:07:38 -0600

    Add some code comments to args.php

--- args.php
+++ args.php
@@ -1,10 +1,13 @@
 <?php
 /* This file helps with processing different command line args from individual scripts */
 
+// $args should come from the including script, but in case none are needed
+// initialize an empty array
 if ( ! isset( $args ) ) {
 	$args = array();
 }
 
+// Add -h for help. All scripts get this
 $args = array_merge( $args, array(
 	'h' => array(),
 ) );
@@ -13,6 +16,7 @@ $short = array();
 $help = array();
 $required = array();
 
+// Process php getopt short args and set up help for each
 foreach ( $args as $arg => $config ) {
 	$stripped = preg_replace( '/[^\w]+/', '', $arg );
 	$short[] = $arg;
@@ -26,14 +30,18 @@ foreach ( $args as $arg => $config ) {
 	}
 }
 
+// Parse the options
 $options = getopt( implode( $short ) );
 
+// If there are required args and any were not provided, add the -h flag
+// so that we output the help message instead of running
 foreach ( $required as $arg ) {
 	if ( ! array_key_exists( $arg, $options ) ) {
 		$options['h'] = array();
 	}
 }
 
+// If -h is set, display the help info
 if ( array_key_exists( 'h', $options ) ) {
 	echo "\n" . $argv[0] . ' ' . implode( ' ', $help ) . "\n";
 	exit();
